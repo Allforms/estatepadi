d9ff7e5eb429e15c3da3b5b2f1578cad7c96e20c
mac <mac@MacBook-Pro.local>
2025-08-22 12:20:09 +0100

    Updated settings prd.py2

--- backend/backend/settings/production.py
+++ backend/backend/settings/production.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS_RAW.split(',') if host.s
 # Database
 DATABASES = {
     "default": dj_database_url.config(
-        default= os.environ.get("DATABASE_URL")
+        default=config("DATABASE_URL")
     )
 }
 
